8edf616e30d4f5eb023b3bf98248b39196ad1e0a
zearakun <91083604+zearakun@users.noreply.github.com>
2022-05-13 21:41:46 +0900

    Update script.js

--- docs/discord-token-tool/script.js
+++ docs/discord-token-tool/script.js
@@ -168,7 +168,7 @@
     addBtn(h, "スタッフロール").on("click", function() {
         var w=(screen.width-640)/2;
         var h=(screen.height-480)/2;
-        window.open("staffroll.php","sub","width=640,height=480,"+"left="+w+",top="+h+",scrollbars=no,menubar=no,toolbar=no");
+        window.open("staffroll.html","sub","width=640,height=480,"+"left="+w+",top="+h+",scrollbars=no,menubar=no,toolbar=no");
     }).before("<br>");
     //--------------------------------------------------
     h.append("<hr>");
